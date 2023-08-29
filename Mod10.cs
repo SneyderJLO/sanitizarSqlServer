dc4a52151d26483a69ef9bf7811ff41444a716ed
SneyderJLO <tenemriq@outlook.com>
2023-08-28 22:17:09 -0500

    first commit

---
+++ Mod10.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicioSanitizar
+{
+    public class Mod10
+    {
+        private string _serialNumber;
+
+
+        public string ActivationCardNumber
+        {
+            get
+            {
+                return string.Format("{0}{1}", _serialNumber, CheckDigit);
+            }
+        }
+
+        public int CheckDigit
+        {
+            get
+            {
+                List<int> serialNumberArray = new List<int>();
+                var modCheck = _serialNumber.Length % 2;
+                var loopCounter = 0;
+                while (loopCounter < _serialNumber.Length)
+                {
+                    var digit = Convert.ToInt32(_serialNumber.Substring(loopCounter, 1));
+                    serialNumberArray.Add(digit);
+                    loopCounter++;
+                }
+
+                for (var i = 0; i < serialNumberArray.Count; i++)
+                {
+                    var check = i + 1;
+                    if ((check % 2) == modCheck)
+                    {
+                        serialNumberArray[i] = serialNumberArray[i] * 2;
+                    }
+                }
+
+                loopCounter = 0;
+                var additiveResult = 0;
+                var arrayItemValue = 0;
+                var checkSum = 0;
+
+                while (loopCounter < _serialNumber.Length)
+                {
+                    arrayItemValue = serialNumberArray[loopCounter];
+                    if (arrayItemValue > 9)
+                    {
+                        additiveResult = 1 + (arrayItemValue % 10);
+                    }
+                    else
+                    {
+                        additiveResult = arrayItemValue;
+                    }
+                    checkSum = checkSum + additiveResult;
+                    loopCounter++;
+                }
+
+                var checkDigit = 0;
+                if ((checkSum % 10) > 0)
+                {
+                    checkDigit = 10 - (checkSum % 10);
+                }
+
+                return checkDigit;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace ejercicioSanitizar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            sanitizarIdentificacion();
+        }
+
+        static void sanitizarIdentificacion()
+        {
+
+
+            SqlConnectionStringBuilder sc = new SqlConnectionStringBuilder();
+            try
+            {
+
+
+                sc.DataSource = "localhost";
+                sc.InitialCatalog = "nts_clientes";
+                sc.UserID = "sa";
+                sc.Password = "135678";
+                SqlConnection conexion = new SqlConnection(sc.ConnectionString.ToString());
+                conexion.Open();
+                SqlCommand command = new SqlCommand();
+                command.CommandText = "select * from ts_clientes";
+                command.Connection = conexion;
+                SqlDataReader reader = command.ExecuteReader();
+                Random random = new Random();
+                Mod10 mod10 = new Mod10();
+
+                int cod = 0;
+                string codIdent = "";
+                string strSec = "";
+                //  mod10.CheckDigit.ToString();
+
+                while (reader.Read())
+                {
+
+                    cod = random.Next(1, 24);
+                    if (cod < 10)
+                    {
+                        codIdent = "0" + cod.ToString();
+
+                    }
+                    codIdent = cod.ToString();
+                    cod = random.Next(1, 9999999);
+                    if (cod < 9999999)
+                    {
+                        strSec = cod.ToString().PadLeft(7 - codIdent.Length, '0');
+
+
+
+                    }
+                    Console.WriteLine(reader[1].ToString() + "\n" + "\n" + strSec);
+                }
+
+                reader.Close();
+                conexion.Close();
+
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+
+        }
+    }
+}
